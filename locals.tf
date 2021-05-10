b58eb1ae8fba49a3395fc6dcac48ee27708540aa
Gonzalo Villafañe Tapia <gvillafanetapia@gmail.com>
2021-05-10 18:57:32 -0300

    Fix cw group var

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
   artifact_bucket_id                 = local.create_dedicated_bucket ? aws_s3_bucket.default.0.id : var.artifact_bucket_id
   ecs_execution_role_arn             = var.create_iam_roles ? aws_iam_role.ecs_execution.0.arn : var.ecs_execution_role_arn
   ecs_task_role_arn                  = var.create_iam_roles ? aws_iam_role.ecs_task.0.arn : var.ecs_task_role_arn
-  cloudwatch_log_group_external_name = var.cloudwatch_log_group_external_name ? var.cloudwatch_log_group_external_name : aws_cloudwatch_log_group.mlflow.arn
+  cloudwatch_log_group_external_name = var.cloudwatch_log_group_external_name ? var.cloudwatch_log_group_external_name : aws_cloudwatch_log_group.mlflow.0.name
   tags = merge(
     {
       "terraform-module" = "glovo/mlflow/aws"
