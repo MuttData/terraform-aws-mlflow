39a88c9cf57fce9b729e813a9a398b7bdf88ffdb
Gonzalo Villafañe Tapia <gvillafanetapia@gmail.com>
2021-05-14 16:43:43 -0300

    Fix mlflow_launch_template_user_data name

--- server.tf
+++ server.tf
@@ -176,7 +176,7 @@ resource "aws_launch_template" "mlflow" {
   image_id               = data.aws_ami.ecs_optimized_ami_linux.0.id
   instance_type          = var.ec2_template_instance_type
   vpc_security_group_ids = [local.ecs_security_group_id]
-  user_data              = base64encode(data.template_file.test.rendered)
+  user_data              = base64encode(data.template_file.mlflow_launch_template_user_data.rendered)
   iam_instance_profile {
     name = "ecsInstanceRole"
   }
