ea8f25db7f2258b04f423ed1e1afcf38d7d398c8
Gonzalo Villafañe Tapia <gvillafanetapia@gmail.com>
2021-05-18 18:54:36 -0300

    Fix mlflow_launch_template_user_data

--- server.tf
+++ server.tf
@@ -164,9 +164,9 @@ data "template_file" "mlflow_launch_template_user_data" {
   template = <<EOF
 #!/bin/bash
 # The cluster this agent should check into.
-echo 'ECS_CLUSTER=${var.unique_name}' >> /etc/ecs/ecs.config
+echo ECS_CLUSTER=${var.unique_name} >> /etc/ecs/ecs.config
 # Disable privileged containers.
-echo 'ECS_DISABLE_PRIVILEGED=true' >> /etc/ecs/ecs.config
+echo ECS_DISABLE_PRIVILEGED=true >> /etc/ecs/ecs.config
 EOF
 }
 
