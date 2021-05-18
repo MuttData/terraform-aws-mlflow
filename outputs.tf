1da3464c6ec1a048acf42a04b01bc2f2a9ae8f42
Gonzalo Villafañe Tapia <gvillafanetapia@gmail.com>
2021-05-18 17:32:55 -0300

    Fix output load balancer only if EC2

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "load_balancer_arn" {
-  value = aws_lb.mlflow.arn
+  value = length(aws_lb.mlflow) > 0 ? aws_lb.mlflow.0.arn
 }
 
 output "load_balancer_target_group_id" {
-  value = aws_lb_target_group.mlflow.id
+  value = length(aws_lb_target_group.mlflow) > 0 ? aws_lb_target_group.mlflow.0.id
 }
 
 output "load_balancer_zone_id" {
-  value = aws_lb.mlflow.zone_id
+  value = length(aws_lb.mlflow) > 0 ? aws_lb.mlflow.0.zone_id
 }
 
 output "load_balancer_dns_name" {
-  value = aws_lb.mlflow.dns_name
+  value = length(aws_lb.mlflow) > 0 ? aws_lb.mlflow.dns_name
 }
 
 output "cluster_id" {
